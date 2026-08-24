d85e2c4826dd9b3efdf6bd01f6ea998b6e9b9c47
sismiko <34129582+sismiko@users.noreply.github.com>
2026-08-24 23:11:01 +0000

    Add files via upload

---
+++ instala_warsaw.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ "$EUID" = 0 ]]; then
+    echo "(1) already root"
+else
+    sudo -k # make sure to ask for password on next sudo ✱
+    if sudo true; then
+        echo "(2) correct password"
+    else
+        echo "(3) wrong password"
+        exit 1
+    fi
+fi
+
+echo '### Instalando o Modulo Warsaw ###'
+rpm -ivh --nodigest warsaw-2.22.1-6.x86_64.rpm
+
+echo '### Configurando Servicos ###'
+systemctl stop warsaw.service
+execstack -s /usr/local/bin/warsaw/core
+chattr +i /usr/local/bin/warsaw/core
+chattr +a /usr/local/bin/warsaw
+systemctl start warsaw.service
+
+echo '### Gravando Certificado Auto Assinado ###'
+sleep 30s
+cp /usr/local/etc/warsaw/rootca.crt /etc/pki/ca-trust/source/anchors/warsaw-ca.crt
+update-ca-trust
+
+echo '### Modulo Instalado ###'
+
+exit 0
